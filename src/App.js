53da834cd68d0645b619fcd0654208821201423c
Doomfang3 <saubinm@free.fr>
2019-03-21 17:38:12 +0100

    create-react-app integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainText from './components/MainText';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <body>
+      <header>
+        <nav class="navContainer">
+        <div></div>
+        <div class="navlink"><a href="#home">Home</a></div>
+        <div class="navlink"><a href="#illus">Illustrations</a></div>
+        <div class="navlink"><a href="#dl">Téléchargements</a></div>
+        <div class="navlink"><a href="#contacts">Contacts</a></div>
+        <div></div>
+        </nav>
+      </header>
+
+      <div class="container">
+        <div></div>
+          <MainText />
+        <div></div>
       </div>
+      <footer></footer>
+      </body>
     );
   }
 }
---
+++ src/data/book_1/chapter_1.js
@@ -0,0 +1,18 @@
+[{"text": "<h1>Chapitre I</h1>
+      <p class='text-dialogue'> Et c'est ainsi que le Démon mi-homme mi-renard à neuf queues fut repoussé au plus profond des grottes de Payon, scellé à tout jamais dans la Cité des Morts. Le village redevint un lieu paisible et la vie reprit son cours.</p>
+  <p>Assis dans l'herbe en demi-cercle autour de leur professeur, les enfants du petit village de Payon étaient suspendus à ses lèvres. C'était leur histoire préférée. Ils applaudirent et la réclamèrent une nouvelle fois.</p>
+  <p class='text-dialogue'> C'est tout pour aujourd'hui. Il est temps de rentrer chez vous !</p>
+  <p class='text-dialogue'> Monsieur, est-ce qu'il est toujours dans la grotte le démon ? demanda une petite fille rousse de cinq ans.</p>
+  <p class='text-dialogue'> Oui ma petite Xi. Mais il est prisonnier. Les magiciens et les prêtres les plus puissants du royaume de Rune Midgard l'ont enfermé dans la Cité des Morts il y a bien longtemps.</p>
+  <p class='text-dialogue'> Mais un jour il risque de ressortir alors ? insista-t-elle.</p>
+  <p class='text-dialogue'> J'espère que non, mais si ça reproduit nous le mettrons de nouveau hors d'état de nuire. Les meilleurs chasseurs ne sont-ils pas formés ici, à Payon ? Tu voudrais devenir chasseur quand tu seras grande ?</p>
+  <p class='text-dialogue'> Ah non, j'aime pas ça les arcs... Je préfèrerai avoir une grosse épée pour défendre les gens contre les méchants monstres ! affirma Xi d'un air solennel.</p>
+  <p class='text-dialogue'> Nous verrons cela, dit l'homme en riant. Allez, rentre vite chez tes parents, ils vont se faire du soucis...</p>
+  <p class='text-dialogue'> D'accord. A demain monsieur ! dit la fillette en quittant l'école.</p>
+  <p>Elle courut sans s'arrêter jusqu'à la maison de ses parents, de braves paysans qui l'avaient adoptée quand elle avait environ trois ans. Ils l’élevaient et la considéraient comme si elle était leur chair et leur sang.</p>
+  <p class='text-sous-p'>Après avoir dîné, Xi monta se coucher dans sa petite chambre. Ses parents lui souhaitèrent bonne nuit, et elle s'endormit. Quelques heures plus tard, sa mère fit irruption dans sa chambre, alertée par des cris de détresse.</p>
+  <p class='text-dialogue'> Tu as encore fait un mauvais rêve, Xi ? dit-elle en la serrant dans ses bras pour la rassurer. Là, calme toi...</p>
+  <p>Elle lui chantonna une berceuse, et la fillette se rendormit.</p>
+  <p class='text-dialogue'> Toujours la même chose, dit-elle à son mari en se recouchant. J'ignore ce qu'elle a vécu avant que nous l'adoptions, mais je suis persuadée que ses cauchemars viennent de là.</p>
+  <p>Le lendemain matin, Xi ne se rappela de rien, comme à chaque fois. Sa mère lui fit deux nattes bien serrées avant de la laisser partir à l'école, la seule coiffure que la petite arrivait à peu près à garder jusqu'au soir.</p>"}
+];
